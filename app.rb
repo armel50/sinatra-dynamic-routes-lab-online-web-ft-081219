2acda20420fcc680cc639dcbd8992e33a5a39be3
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-25 19:55:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,18 +4,22 @@ class App < Sinatra::Base
   
   get  '/reversename/:name' do 
     @reversename = params[:name].reverse
+    erb :reverse
   end
   
   get '/square/:number' do 
     @square = params[:number].to_i ** 2
+    erb :square
   end 
   
   get '/say/:number/:phrase' do 
       @array = Array.new(params[:number], params[:phrase])
+      erb :say
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @string = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
+    erb :say_word
   end 
   
   get '/:operation/:number1/:number2' do 
@@ -33,7 +37,7 @@ class App < Sinatra::Base
       when "divide"
         @result = @number1.to_i / @number2.to_i
     end
-  
+    erb :operation
   end
 
 end
